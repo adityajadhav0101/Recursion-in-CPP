5196f3b32563f239d252032d8f3bc9be6ef39ec1
Aditya Jadhav <adityabjadhav1252@gmail.com>
2025-09-22 20:18:00 +0530

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,25 @@
+// Aditya Jadhav
+// 24070123005
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if (n==0){
+        return 1;
+    }
+    else return n*fact(n-1);
+}
+
+int main()
+{
+    int a;
+    cout<<"Enter an integer: "<<endl;
+    cin>>a;
+    cout<<"Factorial is: "<<fact(a);
+}
+
+// Output
+// Enter an integer: 
+// 5
+// Factorial is: 120
